fca93b1c9924e5cdc7d820d6454ffa0d383ee64b
leslie bienenfeld <lbienenfeld@gmail.com>
2014-09-06 02:03:37 -0700

    added sourcery and index file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyChortl::Application.config.secret_token = '14db3a33f13bb0952f608974ac42e4a843d7cede498c7d2aed2bb168b62b30849a99a27092074aef8ff8b1450d34c540347220df9382452a60cc1fb9775fc926'
