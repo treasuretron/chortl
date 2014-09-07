c12c4f2ba2dac90852039f37adc08a61043c3478
Tim Borny <bornytm@gmail.com>
2014-09-06 18:01:32 -0700

    add new household

--- handlers.js
+++ handlers.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 mongoose.connect('mongodb://localhost/test');
 
-
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
@@ -11,23 +10,25 @@ db.once('open', function callback () {
 
 
 var householdSchema = new Schema({
-  admin: String,
   name:  String,
   occupants: [housemateSchema],
   chores: [choreTypeSchema],
-  day: Number,
-  week: Number
+  // day: Number,
+  // week: Number
 });
+var Household = mongoose.model('Household', householdSchema);
 
 var housemateSchema = new Schema( {
   name: String, 
   email: String
-})
+});
+var Housemate = mongoose.model('Housemate', housemateSchema);
 
 var choreTypeSchema = new Schema({
   name: String,
   description: String
-})
+});
+var ChoreType = mongoose.model('ChoreType', choreTypeSchema);
 
 var choreSchema = new Schema ({
   housemate : [housemateSchema],
@@ -35,13 +36,53 @@ var choreSchema = new Schema ({
   date : Date,
   karma : Number,
   house : [householdSchema]
-})
+});
+var Chore = mongoose.model('Chore', choreSchema);
 
+// add a new household, the occupants, and relevant chores
 exports.newHousehold = function (req, res){
-  console.log(req.body);
+  
+  var data = req.body;
+
+  // TODO: make sure house name is unique!
+
+  // add household
+  var newHouse = new Household({
+      name:  data.name,
+  });
+
+  // add chores
+  for(var i = 0; i < data.chores.length; i++){
+    newHouse.chores.push({
+      name: data.chores[i].name,
+      description: data.chores[i].email
+    });
+  }
+
+
+  // add housemates
+  for(var i = 0; i < data.housemates.length; i++){
+    newHouse.occupants.push({
+      name: data.housemates[i].name,
+      email: data.housemates[i].email
+    });
+  }
+
+  // save new household
+  newHouse.save(function (err) {
+    if (err) return handleError(err);
+    console.log('Success!');
+    res.send(); // send response
+  });
+  
 };
 
-var household = mongoose.model('household', householdSchema);
+// return household info (people, chores, name...)
+exports.getHousehold = function (req, res){
+  // fetch data based on id provided
+
+  //return the data
+};
 
 exports.sendEmail = function (req, res){
   console.log("the EMAIL!: ");
